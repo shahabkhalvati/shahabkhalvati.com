c30a8d419e368e2e28e003015e73c96eeec2ac3d
Shahab Khalvati <contact@shahabkhalvati.com>
2023-06-28 20:09:36 +0200

    redirect?

--- next.config.js
+++ next.config.js
@@ -2,10 +2,19 @@ const nextConfig = {
   experimental: {
     mdxRs: true,
   },
-  webpack: (config) => {
+  webpack: config => {
     config.resolve.fallback = { fs: false, paths: false }
 
     return config
+  },
+  async redirects() {
+    return [
+      {
+        source: '/favorites',
+        destination: '/collection/favorites',
+        permanent: false,
+      },
+    ]
   },
 }
 
